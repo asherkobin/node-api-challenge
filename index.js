c31e8a9ea18c0eee6ae3f70316d60268fd3253a3
Asher Kobin <asherkobin@gmail.com>
2020-02-14 13:49:17 -0500

    odds and ends

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const server = express();
 const portNum = 5050;
 const actionsRouter = require("./routers/actionsRouter");
 const projectsRouter = require("./routers/projectsRouter");
+const cors = require("cors");
 
 server.use(express.json());
+server.use(cors());
 
 server.use("/api/actions", actionsRouter);
 server.use("/api/projects", projectsRouter);
